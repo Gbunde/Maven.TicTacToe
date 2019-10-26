ad840aa2152709bcc3e3ddc0d3ce64f5926321f3
Gbunde <56592891+Gbunde@users.noreply.github.com>
2019-10-26 18:15:18 -0400

    finished lab

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,92 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    public Character[][] tTacBoard;
+
     public Board(Character[][] matrix) {
+        this.tTacBoard = matrix;
+
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+
+
+        for (int i = 0; i < tTacBoard.length; i++) {
+
+            if (tTacBoard[i][0] == 'X' && tTacBoard[i][1] == 'X' && tTacBoard[i][2] == 'X') {
+
+                return true;
+            }
+            if (tTacBoard[0][i] == 'X' && tTacBoard[1][i] == 'X' && tTacBoard[2][i] == 'X') {
+                return true;
+            }
+        }
+        if (tTacBoard[0][0] == 'X' && tTacBoard[1][1] == 'X' && tTacBoard[2][2] == 'X') {
+
+            return true;
+        }
+        if (tTacBoard[0][2] == 'X' && tTacBoard[1][1] == 'X' && tTacBoard[2][0] == 'X') {
+            return true;
+        }
+        return false;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        for (int i = 0; i < tTacBoard.length; i++) {
+
+            if (tTacBoard[i][0] == 'O' && tTacBoard[i][1] == 'O' && tTacBoard[i][2] == 'O') {
+                return true;
+            }
+            if (tTacBoard[0][i] == 'O' && tTacBoard[1][i] == 'O' && tTacBoard[2][i] == 'O') {
+                return true;
+            }
+        }
+            if (tTacBoard[0][0] == 'O' && tTacBoard[1][1] == 'O' && tTacBoard[2][2] == 'O') {
+                return true;
+            }
+            if (tTacBoard[0][2] == 'O' && tTacBoard[1][1] == 'O' && tTacBoard[2][0] == 'O') {
+                return true;
+
+        }
+        return false;
     }
 
     public Boolean isTie() {
-        return null;
+
+
+        for (int i = 0; i < tTacBoard.length; i++) {
+
+
+            for (int j = 0; j < tTacBoard.length; j++) {
+
+
+                if (tTacBoard[i][j] == ' ') {
+                    return false;
+                }
+
+            }
+
+        }
+        return true;
     }
 
-    public String getWinner() {
-        return null;
+    public String getWinner () {
+
+            if (isInFavorOfX() == true) {
+
+                return "X";
+            }
+
+            if (isInFavorOfO()== true) {
+
+                return "O";
+
+            }
+
+        return "";
+
     }
 
 }
+
+
